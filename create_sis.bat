8ee54b6d90414fa18bd150af98553c79c061097f
marcelobarrosalmeida <marcelobarrosalmeida@3eea4c62-fc83-11dd-802b-177316e4ac06>
2009-03-14 22:38:29 +0000

    changind version number

--- create_sis.bat
+++ create_sis.bat
@@ -5,7 +5,7 @@ SET APPNAME=Iamdoing
 SET CAPBLS=NetworkServices+LocalServices+ReadUserData+WriteUserData+UserEnvironment
 SET SRCDIR=src
 SET TMPDIR=src.tmp
-SET VER=0.0.1
+SET VER=0.0.5
 IF NOT EXIST %TMPDIR% mkdir %TMPDIR%
 
 copy  %SRCDIR%\*.py  %TMPDIR%
