45cfa52c0e065d9e390f9691298a5cec78f3126b
marcelobarrosalmeida <marcelobarrosalmeida@3eea4c62-fc83-11dd-802b-177316e4ac06>
2009-03-15 14:54:50 +0000

    version 0.0.6

--- trunk/create_sis.bat
+++ trunk/create_sis.bat
@@ -5,7 +5,7 @@ SET APPNAME=Iamdoing
 SET CAPBLS=NetworkServices+LocalServices+ReadUserData+WriteUserData+UserEnvironment
 SET SRCDIR=src
 SET TMPDIR=src.tmp
-SET VER=0.0.5
+SET VER=0.0.6
 IF NOT EXIST %TMPDIR% mkdir %TMPDIR%
 
 copy  %SRCDIR%\*.py  %TMPDIR%
--- trunk/src/wmglobals.py
+++ trunk/src/wmglobals.py
@@ -3,7 +3,7 @@
 __all__ = [ "VERSION", "DEFDIR" ]
 
 # always 3 numbers with two digits each maximum, e.g. 3.44.2, 4.2.33 ...
-VERSION = "0.0.5" 
+VERSION = "0.0.6" 
 DEFDIR = "e:\\iamdoing\\"
 
 
