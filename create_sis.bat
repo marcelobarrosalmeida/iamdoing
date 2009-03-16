c4e32dc9f7733bae46e1dc63e04e5d48172ea17f
marcelobarrosalmeida <marcelobarrosalmeida@3eea4c62-fc83-11dd-802b-177316e4ac06>
2009-03-16 17:33:31 +0000

    proxy support and http basic authentication with urllib

--- create_sis.bat
+++ create_sis.bat
@@ -5,7 +5,7 @@ SET APPNAME=Iamdoing
 SET CAPBLS=NetworkServices+LocalServices+ReadUserData+WriteUserData+UserEnvironment
 SET SRCDIR=src
 SET TMPDIR=src.tmp
-SET VER=0.0.7
+SET VER=0.0.8
 IF NOT EXIST %TMPDIR% mkdir %TMPDIR%
 
 copy  %SRCDIR%\*.py  %TMPDIR%
--- src/wmglobals.py
+++ src/wmglobals.py
@@ -3,7 +3,7 @@
 __all__ = [ "VERSION", "DEFDIR", "PERSIST", "AVATARSDIR" ]
 
 # always 3 numbers with two digits each maximum, e.g. 3.44.2, 4.2.33 ...
-VERSION = "0.0.7" 
+VERSION = "0.0.8" 
 DEFDIR = "e:\\iamdoing\\"
 PERSIST = "persist.bin"
 AVATARSDIR = DEFDIR + "avatars"
