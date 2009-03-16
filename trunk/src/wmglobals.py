ef7db4ca2d4fa6ba4553f5111d9e826422f15bb7
marcelobarrosalmeida <marcelobarrosalmeida@3eea4c62-fc83-11dd-802b-177316e4ac06>
2009-03-16 05:21:13 +0000

    local for caching avatars

--- trunk/src/wmglobals.py
+++ trunk/src/wmglobals.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-__all__ = [ "VERSION", "DEFDIR" ]
+__all__ = [ "VERSION", "DEFDIR", "PERSIST", "AVATARSDIR" ]
 
 # always 3 numbers with two digits each maximum, e.g. 3.44.2, 4.2.33 ...
 VERSION = "0.0.6" 
 DEFDIR = "e:\\iamdoing\\"
+PERSIST = "persist.bin"
+AVATARSDIR = DEFDIR + "avatars"
 
 
